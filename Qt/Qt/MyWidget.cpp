f0bee9488d893e36dfc6548f911321dc7dbcac0d
xuanming.tao <2037195653@qq.com>
2026-04-20 17:37:15 +0800

    feat: Qt组合控件新增自定义窗口组件

---
+++ Qt/Qt/MyWidget.cpp
@@ -0,0 +1,31 @@
+﻿#include "MyWidget.h"
+#include<QDebug>
+#include<QVBoxLayout>
+#include "GroupWidget.h"
+MyWidget::MyWidget(QWidget* parent):QWidget(parent)
+{
+    resize(640, 480);
+    // 直接new你的自定义组合控件
+    GroupWidget* group = new GroupWidget(this);
+
+    // 绑定组合控件的自定义信号
+    connect(group, &GroupWidget::btnClicked, this, [=]() {
+        qDebug() << "组合控件按钮点击：" << group->getText();
+        });
+
+}
+
+MyWidget::~MyWidget()
+{
+
+}
+
+void MyWidget::mousePressEvent(QMouseEvent* ev)
+{
+
+}
+
+void MyWidget::paintEvent(QPaintEvent* event)
+{
+   
+}
--- Qt/Qt/main.cpp
+++ Qt/Qt/main.cpp
@@ -1,4 +1,10 @@
-#include<iostream>
-int main() {
-	std::cout << "qt运行成功";
+#include "MyWidget.h"
+#include <QApplication>
+
+int main(int argc, char* argv[])
+{
+    QApplication a(argc, argv);
+    MyWidget w;
+    w.show();
+    return a.exec();
 }
